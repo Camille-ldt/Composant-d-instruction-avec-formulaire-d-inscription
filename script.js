eafcb884409bb20adea1677522523e90ae43b2e0
Camille6313 <camille.laidet63@gmail.com>
2024-09-09 13:06:11 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const containerRight = document.querySelector('.container-right');
+console.log(containerRight);
+
+
+
+
+    const inputContainer = document.createElement(`form`);
+    inputContainer.classList = `configuration`;
+    containerRight.appendChild(inputContainer);
+    
+    const inputFirstName = document.createElement(`input`);
+    inputFirstName.type = `text`;
+    inputFirstName.id = `inputContent`;
+    inputFirstName.name = `First Name`;
+    inputFirstName.placeholder = "First Name";
+    inputContainer.appendChild(inputFirstName);
+    
+    const inputLastName = document.createElement(`input`); 
+    inputLastName.type = `text`;
+    inputLastName.id = `inputContent`;
+    inputLastName.name = `Last Name`;
+    inputLastName.placeholder = "Last Name";
+    inputContainer.appendChild(inputLastName);
+    
+    const inputEmailAdress = document.createElement(`input`);
+    inputEmailAdress.type = `text`; 
+    inputEmailAdress.id = `inputContent`;
+    inputEmailAdress.name = `Email Adress`;
+    inputEmailAdress.placeholder = "Email Adress";
+    inputContainer.appendChild(inputEmailAdress);
+    
+    const inputPasseword = document.createElement(`input`);
+    inputPasseword.type = `text`;
+    inputPasseword.id = `inputContent`;
+    inputPasseword.name = `Passeword`;
+    inputPasseword.placeholder = "Passeword";
+    inputContainer.appendChild(inputPasseword);
+
+    const inputButton = document.createElement(`input`);
+    inputButton.type = `submit`;
+    inputButton.id = `inputBtn`;
+    inputButton.name = `Button`;
+    inputButton.value = "Claim your free trial";
+    inputContainer.appendChild(inputButton);
+
